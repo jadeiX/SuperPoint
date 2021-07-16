d2a854e6a5d4cc8684ee0d85f5c4e243e6e23116
Jade Balla <jade@jadei.dev>
2021-07-16 21:00:23 +0000

    update code for only kp and desc

--- superpoint/extract_keypoints_descriptors.py
+++ superpoint/extract_keypoints_descriptors.py
@@ -1,4 +1,3 @@
-import argparse
 from pathlib import Path
 
 import cv2
@@ -7,12 +6,11 @@ import tensorflow as tf  # noqa: E402
 
 from settings import EXPER_PATH  # noqa: E402
 
-def extract_superpoint_keypoints_and_descriptors(keypoint_map, descriptor_map,
-                                                 keep_k_points=1000):
 
+def extract_superpoint_keypoints_and_descriptors(keypoint_map, descriptor_map, keep_k_points):
     def select_k_best(points, k):
-        """ Select the k most probable points (and strip their proba).
-        points has shape (num_points, 3) where the last coordinate is the proba. """
+        """Select the k most probable points (and strip their proba).
+        points has shape (num_points, 3) where the last coordinate is the proba."""
         sorted_prob = points[points[:, 2].argsort(), :2]
         start = min(k, points.shape[0])
         return sorted_prob[-start:, :]
@@ -34,99 +32,62 @@ def extract_superpoint_keypoints_and_descriptors(keypoint_map, descriptor_map,
     return keypoints, desc
 
 
-def preprocess_image(img_file, img_size):
+def preprocess_image(img_file, img_size, resize_image):
     img = cv2.imread(img_file, cv2.IMREAD_COLOR)
-    img = cv2.resize(img, img_size)
+    if resize_image:
+        img = cv2.resize(img, img_size)
     img_orig = img.copy()
 
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = np.expand_dims(img, 2)
     img = img.astype(np.float32)
-    img_preprocessed = img / 255.
+    img_preprocessed = img / 255.0
 
     return img_preprocessed, img_orig
 
 
+def get_superpoint_kp_desc(
+    img_file,
+    weights_name: str = "sp_v6",
+    resize_image: bool = True,
+    height: int = 480,
+    width: int = 640,
+    keep_kp_best: int = 1000,
+):
+    """
+    superpoint_generator [summary]
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser = argparse.ArgumentParser(description='Compute the homography \
-            between two images with the SuperPoint feature matches.')
-    parser.add_argument('weights_name', type=str)
-    parser.add_argument('img1_path', type=str)
-    parser.add_argument('img2_path', type=str)
-    parser.add_argument('--H', type=int, default=480,
-                        help='The height in pixels to resize the images to. \
-                                (default: 480)')
-    parser.add_argument('--W', type=int, default=640,
-                        help='The width in pixels to resize the images to. \
-                                (default: 640)')
-    parser.add_argument('--k_best', type=int, default=1000,
-                        help='Maximum number of keypoints to keep \
-                        (default: 1000)')
-    args = parser.parse_args()
+    Args:
+        img_file ([type]): [description]
+        weights_name (str, optional): [weights_name]. Defaults to "sp_v6".
+        resize_image (bool, optional): [whether to resize image]. Defaults to True.
+        height (int, optional): [The height in pixels to resize the images to]. Defaults to 480.
+        width (int, optional): [The width in pixels to resize the images to]. Defaults to 640.
+        keep_kp_best (int, optional): [Maximum number of keypoints to keep]. Defaults to 1000.
+    """
+    img_size = (width, height)
 
-    weights_name = args.weights_name
-    img1_file = args.img1_path
-    img2_file = args.img2_path
-    img_size = (args.W, args.H)
-    keep_k_best = args.k_best
-
-    weights_root_dir = Path(EXPER_PATH, 'saved_models')
+    weights_root_dir = Path(EXPER_PATH, "saved_models")
     weights_root_dir.mkdir(parents=True, exist_ok=True)
     weights_dir = Path(weights_root_dir, weights_name)
 
     graph = tf.Graph()
     with tf.compat.v1.Session(graph=graph) as sess:
-        tf.compat.v1.saved_model.loader.load(sess,
-                                   [tf.saved_model.SERVING],
-                                   str(weights_dir))
+        tf.compat.v1.saved_model.load(sess, [tf.saved_model.SERVING], str(weights_dir))
 
-        input_img_tensor = graph.get_tensor_by_name('superpoint/image:0')
-        output_prob_nms_tensor = graph.get_tensor_by_name('superpoint/prob_nms:0')
-        output_desc_tensors = graph.get_tensor_by_name('superpoint/descriptors:0')
+        input_img_tensor = graph.get_tensor_by_name("superpoint/image:0")
+        output_prob_nms_tensor = graph.get_tensor_by_name("superpoint/prob_nms:0")
+        output_desc_tensors = graph.get_tensor_by_name("superpoint/descriptors:0")
 
-        img1, img1_orig = preprocess_image(img1_file, img_size)
-        out1 = sess.run([output_prob_nms_tensor, output_desc_tensors],
-                        feed_dict={input_img_tensor: np.expand_dims(img1, 0)})
-        keypoint_map1 = np.squeeze(out1[0])
-        descriptor_map1 = np.squeeze(out1[1])
-        kp1, desc1 = extract_superpoint_keypoints_and_descriptors(
-                keypoint_map1, descriptor_map1, keep_k_best)
+        img, img_orig = preprocess_image(img_file, img_size, resize_image)
+        out = sess.run(
+            [output_prob_nms_tensor, output_desc_tensors], feed_dict={input_img_tensor: np.expand_dims(img, 0)}
+        )
+        keypoint_map = np.squeeze(out[0])
+        descriptor_map = np.squeeze(out[1])
+        kp, desc = extract_superpoint_keypoints_and_descriptors(keypoint_map, descriptor_map, keep_kp_best)
+    return kp, desc
 
-        img2, img2_orig = preprocess_image(img2_file, img_size)
-        out2 = sess.run([output_prob_nms_tensor, output_desc_tensors],
-                        feed_dict={input_img_tensor: np.expand_dims(img2, 0)})
-        keypoint_map2 = np.squeeze(out2[0])
-        descriptor_map2 = np.squeeze(out2[1])
-        kp2, desc2 = extract_superpoint_keypoints_and_descriptors(
-                keypoint_map2, descriptor_map2, keep_k_best)
-
-        # Match and get rid of outliers
-        m_kp1, m_kp2, matches = match_descriptors(kp1, desc1, kp2, desc2)
-        H, inliers = compute_homography(m_kp1, m_kp2)
-
-        # Draw SuperPoint matches
-        matches = np.array(matches)[inliers.astype(bool)].tolist()
-        matched_img = cv2.drawMatches(img1_orig, kp1, img2_orig, kp2, matches,
-                                      None, matchColor=(0, 255, 0),
-                                      singlePointColor=(0, 0, 255))
-
-        cv2.imshow("SuperPoint matches", matched_img)
-
-        # Compare SIFT matches
-        sift_kp1, sift_desc1 = extract_SIFT_keypoints_and_descriptors(img1_orig)
-        sift_kp2, sift_desc2 = extract_SIFT_keypoints_and_descriptors(img2_orig)
-        sift_m_kp1, sift_m_kp2, sift_matches = match_descriptors(
-                sift_kp1, sift_desc1, sift_kp2, sift_desc2)
-        sift_H, sift_inliers = compute_homography(sift_m_kp1, sift_m_kp2)
-
-        # Draw SIFT matches
-        sift_matches = np.array(sift_matches)[sift_inliers.astype(bool)].tolist()
-        sift_matched_img = cv2.drawMatches(img1_orig, sift_kp1, img2_orig,
-                                           sift_kp2, sift_matches, None,
-                                           matchColor=(0, 255, 0),
-                                           singlePointColor=(0, 0, 255))
-        cv2.imshow("SIFT matches", sift_matched_img)
-
-        cv2.waitKey(0)
+if __name__ == "__main__":
+    kp, desc = get_superpoint_kp_desc(img_file="/content/drive/MyDrive/ColabNotebooks/superpoint/data/val2014/COCO_val2014_000000000164.jpg")
+    print(kp[0].pt)
